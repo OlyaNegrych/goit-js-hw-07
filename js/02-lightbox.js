c3f44b1db906046a83df182365a292b161eea450
OlyaNegrych <olyanegrych@gmail.com>
2022-07-22 19:24:44 +0300

    start task 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,9 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+<a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>;
