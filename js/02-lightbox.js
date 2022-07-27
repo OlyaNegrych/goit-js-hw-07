52b3480c27fc360a886e0e3d2ce3cfb4a169babe
OlyaNegrych <olyanegrych@gmail.com>
2022-07-27 23:01:05 +0300

    marked up task 7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,10 +1,45 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryFn(galleryItems);
 
+imageContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+imageContainer.addEventListener('click', onOpenImageModal);
 
+let gallery = new SimpleLightbox('.gallery a');
 
-<a class="gallery__item" href="large-image.jpg">
-  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
-</a>;
+function onOpenImageModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  // gallery.on('show.simplelightbox', function () {
+  // });
+
+  gallery.on('captionsData', function () {
+    gallery.captionsData = event.target.alt;
+  }); 
+
+  gallery.on('captionsDelay', function () {
+    gallery.captionsDelay = 250;
+  });
+
+  gallery.on('captionPosition', function () {
+    gallery.captionPosition = 'bottom';
+  }); 
+   
+}
+
+function createGalleryFn(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+`;
+    })
+    .join('');
+}
